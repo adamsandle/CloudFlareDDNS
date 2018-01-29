387b9289fb807169f3ca52e1f8fac5a61901c9cb
Adam Sandle <18138428+adamsandle@users.noreply.github.com>
2018-01-29 17:47:20 +0000

    Tidy up logger (#2)
    add logic to only fetch ip when needed

--- CloudFlareDDNS/CloudFlareDdnsService.cs
+++ CloudFlareDDNS/CloudFlareDdnsService.cs
@@ -2,15 +2,19 @@
 using System.IO;
 using System.ServiceProcess;
 using System.Timers;
+using CloudFlareDDNS.Models;
 
 namespace CloudFlareDDNS
 {
     public partial class CloudFlareDdnsService : ServiceBase
     {
         private Timer _eventTimer;
+        private IpResponse _ipResponse;
+        private bool _isUpdating;
         public CloudFlareDdnsService()
         {
             InitializeComponent();
+            _isUpdating = false;
         }
 
         protected override void OnStart(string[] args)
@@ -19,22 +23,27 @@ namespace CloudFlareDDNS
             _eventTimer.Interval = 10000;
             _eventTimer.Elapsed += EventTimerTick;
             _eventTimer.Enabled = true;
-            Library.WriteErrorLog("Test windows service started");
+            Logger.WriteLog("Test windows service started");
             string folderName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
                 "CloudFlareDDNS");
             Directory.CreateDirectory(folderName);
         }
 
-        private void EventTimerTick(object sender, ElapsedEventArgs e)
+        private async void EventTimerTick(object sender, ElapsedEventArgs e)
         {
-            Library.GetPublicIp();
-            
+            if ((_ipResponse == null || (DateTime.UtcNow - _ipResponse.Updated).Minutes > 4) && !_isUpdating)
+            {
+                _isUpdating = true;
+                _ipResponse = await Http.GetPublicIp();
+                Logger.WriteLog("Ip updated:" + _ipResponse.Ip);
+                _isUpdating = false;
+            }
         }
 
         protected override void OnStop()
         {
             _eventTimer.Enabled = false;
-            Library.WriteErrorLog("Test service stopped");
+            Logger.WriteLog("Test service stopped");
         }
     }
 }
--- CloudFlareDDNS/Models/IpResponse.cs
+++ CloudFlareDDNS/Models/IpResponse.cs
@@ -1,7 +1,10 @@
-﻿namespace CloudFlareDDNS.Models
+﻿using System;
+
+namespace CloudFlareDDNS.Models
 {
     public class IpResponse
     {
         public string Ip { get; set; }
+        public DateTime Updated { get; set; }
     }
 }
