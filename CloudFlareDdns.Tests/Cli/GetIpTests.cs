d168acc399b7ac1a83df100cb81bccfbf5785986
Adam Sandle <18138428+adamsandle@users.noreply.github.com>
2018-08-12 12:53:31 +0100

    Add unit test project (#14)

---
+++ CloudFlareDdns.Tests/Cli/GetIpTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using CloudFlareDdns.Cli;
+using CloudFlareDdns.SharedLogic.Models;
+using Moq;
+using NUnit.Framework;
+
+namespace CloudFlareDdns.Tests.Cli
+{
+    [TestFixture]
+    public class GetIpTests : CloudFlareDdnsCommsTests
+    {
+        [Test]
+        public void GetIpWorksCorrectly()
+        {
+            _commsService.Setup(x => x.GetIp()).Returns(new GetIpResponse
+            {
+                Success = true,
+                IpAddress = "1.2.3.4"
+            });
+
+            var result = _networkActions.GetIp(new GetIpOptions());
+
+            _commsService.Verify(x => x.GetIp(), Times.Once);
+            _outputService.Verify(x => x.WriteLine("1.2.3.4"), Times.Once);
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void GetIpErrorsCorrectly()
+        {
+            var error = new Exception("Error");
+
+            _commsService.Setup(x => x.GetIp()).Throws(error);
+
+            var result = _networkActions.GetIp(new GetIpOptions());
+
+            _outputService.Verify(x => x.WriteLine(error));
+            Assert.AreEqual(1, result);
+        }
+    }
+}
