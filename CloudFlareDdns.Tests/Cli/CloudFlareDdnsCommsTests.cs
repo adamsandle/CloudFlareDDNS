951817ca15e9669b80d28d9ca33244120c623109
Adam Sandle <18138428+adamsandle@users.noreply.github.com>
2018-08-12 11:48:06 +0100

    Add unit test project

---
+++ CloudFlareDdns.Tests/Cli/CloudFlareDdnsCommsTests.cs
@@ -0,0 +1,23 @@
+﻿using CloudFlareDdns.Cli;
+using CloudFlareDdns.SharedLogic.Interfaces;
+using Moq;
+using NUnit.Framework;
+
+namespace CloudFlareDdns.Tests.Cli
+{
+    [TestFixture]
+    public class CloudFlareDdnsCommsTests
+    {
+        protected Mock<ICloudFlareDdnsCommsService> _commsService;
+        protected Mock<IOutputService> _outputService;
+        protected NetworkActions _networkActions;
+
+        [SetUp]
+        public void Setup()
+        {
+            _commsService = new Mock<ICloudFlareDdnsCommsService>();
+            _outputService = new Mock<IOutputService>();
+            _networkActions = new NetworkActions(_commsService.Object, _outputService.Object);
+        }
+    }
+}
